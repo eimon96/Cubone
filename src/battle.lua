ae728143b76a1c7dea9fccc660ad2e4ac265db6d
eimon96 <>
2023-01-05 19:55:24 +0200

    Battle instructions

--- src/battle.lua
+++ src/battle.lua
@@ -6,6 +6,10 @@ end
 function drawBattleField()
     local background = sprites[91] 
     love.graphics.draw(background)
+    
+    love.graphics.print('1: Tackle', WINDOW_WIDTH - 190, WINDOW_HEIGHT - 90)
+    love.graphics.print('2: Bone Throw', WINDOW_WIDTH - 190, WINDOW_HEIGHT - 60)
+    love.graphics.print('R: Run', WINDOW_WIDTH - 190, WINDOW_HEIGHT - 30)
 end
 
 function drawEnemy()
